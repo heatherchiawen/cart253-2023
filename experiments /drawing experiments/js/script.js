8a429a2d5798f455e171a83eadebd6bea6458a7c
Heather Chester <h_cheste@live.concordia.ca>
2023-09-12 14:24:22 -0400

    Attempting to save changes on both in class mac and laptop.

--- experiments /drawing experiments/js/script.js
+++ experiments /drawing experiments/js/script.js
@@ -1,9 +1,8 @@
 /**
- * Title of Project
- * Author Name
- * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * Drawing Experiments 
+ * Heather Chester
+ 
+ * Experimenting with p5's drawing and colour functions. 
  */
 
 "use strict";
