c1ec316797941470606556812105d0e88ee5f9fe
Heather Chester <h_cheste@live.concordia.ca>
2023-09-15 14:21:48 -0400

    Created a body and some eyes

---
+++ alien activity/js/script.js
@@ -0,0 +1,45 @@
+/**
+ * Draw an Alien Activity 
+ * Heather Chester 
+ * 
+ * Drawing an alien using the drawing functions. 
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+    createCanvas (640, 480);
+    background (100, 10 , 10);
+
+// */Drawing the body 
+    noStroke ();
+    fill (10, 10, 100);
+    ellipse (320, 240, 100, 200);
+
+// */Drawing the eyes 
+
+fill(0, 0, 0);
+
+
+
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
