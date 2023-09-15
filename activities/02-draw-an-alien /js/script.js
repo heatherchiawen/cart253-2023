2799e228b4cd8205caa6fde687ace4f70e388ded
Heather Chester <h_cheste@live.concordia.ca>
2023-09-15 14:29:28 -0400

    trying to get the code to go live

--- activities/02-draw-an-alien /js/script.js
+++ activities/02-draw-an-alien /js/script.js
@@ -1,9 +1,8 @@
 /**
- * Title of Project
- * Author Name
+ * Draw an Alien 
+ * Heather Chester 
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * Using drawing functions to create an alien. 
  */
 
 "use strict";
@@ -12,6 +11,10 @@
  * Description of preload
 */
 function preload() {
+
+    createCanvas (500, 500);
+    background (180, 10 , 10);
+
 
 }
 
