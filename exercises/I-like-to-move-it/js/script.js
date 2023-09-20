a3cf7ef593e2dadf23d20137029dd27864d0f525
Heather Chester <h_cheste@live.concordia.ca>
2023-09-20 15:41:49 -0400

    started exercise #1

--- exercises/I-like-to-move-it/js/script.js
+++ exercises/I-like-to-move-it/js/script.js
@@ -1,9 +1,8 @@
 /**
- * Title of Project
- * Heather Chester
+ * i-like-to-move-it
+ * Heather Chester 
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * Exercise #1, declaring and changing variables with JavaScript objects using map() and constrain().   
  */
 
 "use strict";
@@ -20,19 +19,48 @@ function preload() {
  * Description of setup
 */
 
-let backgroundShade = 0; 
+let bg = {
+    red: 0, 
+    green: 0, 
+    blue: 0
+}; //Created bg for background colour and its properties
 
-let circle = {
 
-    x: 0,
+let rect1 = {
+    x: 0, 
     y: 250,
-    size: 100,
-    speed: 1 
-}; 
+    size: 50, 
+    fill: 255,
+    alpha: 200,
+    speed: 1,
+    growthRate: 1
+}; //Creatrect and its properties
+
+
+let rect2 = {
+    x: 500,
+    y: 250,
+    size: 50,
+    sizeRatio: 0.75,
+    fill: 255, 
+    alpha:200,
+    speed: -1
+}; //Createt rect2 and its properties
+
+let rect3 = {
+    x: 250,
+    y: 0,
+    size: 50,
+    sizeRatio: 0.75,
+    fill: 255, 
+    alpha:200,
+    speed: 1
+}; //Createt rect3 and its properties
+
 
 function setup() {
-
-    createCanvas (windowWidth, windowHeight);
+    createCanvas(500, 500);
+    noStroke();
 
 }
 
@@ -42,9 +70,31 @@ function setup() {
 */
 function draw() {
 
-    background (backgroundShade); 
-    ellipse (circle.x, circle.y, circle.size);
-    circle.x = circle.x + circle.speed; 
+    //Background
+    background(bg.red, bg.green, bg.blue);
+    bg.red = (rect.size, 100, width, 0, 255);
+
+    //Left rect1
+    rect1.x = rect1.x + rect1.speed
+    rect1.x = constrain (rect1.x, 0, width/2);
+    rect1.size = rect1.size + rect1.growthRate;
+    rect1.size = constrain (rect1.size, 0, width)
+    fill(rect1.fill, rect1.alpha);
+    rect (rect1.x, rect1.y, rect1.size);
+
+    //Right rect2 
+    rect2.x = rect2.x + rect2.speed;
+    rect2.x = constrain (rect2.x, 0, width/2);
+    rect2.size = rect1.size * rect2.sizeRatio;
+    fill(rect2.fill, rect2.alpha);
+    rect(rect2.x, rect2.y, rect2.size);
+
+     //Right rect3
+     rect3.x = rect3.x * rect3.y + rect3.speed;
+     rect3.x = constrain (rect3.x, 0, width/2);
+     rect3.size = rect1.size * rect3.sizeRatio;
+     rect3.fill= map(rect3.fill, 100, width, 0, 255);
+     rect(rect3.x, rect3.y, rect3.size);
 
 
 }
