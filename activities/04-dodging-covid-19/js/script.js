b44630cafb06f3029bd696b2a1fabc83e99e1c49
Heather Chester <h_cheste@live.concordia.ca>
2023-09-26 14:10:03 -0400

    Understanding conditionals in class

--- activities/04-dodging-covid-19/js/script.js
+++ activities/04-dodging-covid-19/js/script.js
@@ -1,9 +1,8 @@
 /**
- * Title of Project
- * Author Name
+ * 04-dodging-covid-19
+ * Heather
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * Creating a simple simulation to become familiar with variables for movement, visuals, etc., conditionals for making decisions, new functions (dist(), noLoop(), noCursor()), and using a loop for a visual effect (static). 
  */
 
 "use strict";
@@ -15,12 +14,29 @@ function preload() {
 
 }
 
+let covid19 = {
+    x: 0,
+    y: 250, 
+    size: 100,
+    speed: 5,
+    fill: {
+        r: 250, 
+        g: 100, 
+        b: 100, 
+        a: 255, 
+    },
+    
+    alphaChange: 1, 
+    // alphaAngle:0
+    
+}
 
 /**
  * Description of setup
 */
 function setup() {
-
+    createCanvas (windowWidth, windowHeight);
+    angleMode (DEGREES);
 }
 
 
@@ -28,5 +44,26 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(172, 242, 178);
+
+    //let sinValue = sin(square.alphaAngle);
+    //square.fill.a = map (sinValue, -1, 1, 0, 255);
+
+    square.fill.a += square.alphaChange;
+    
+    if (square.fill.a >= 255); {
+        square.alphaChange *= -1; 
+    }
+
+    else if (square.fill.a >= 255) {
+        square.alphaChange *= -1; 
+    }
+    
+    rectMODE(CENTER);
+    noStroke();
+    fill(square.fill.r, square.fill.g, square.fill.b, square.fill.a);
+    rect(square.x, square.y, square.size);
+
+    // square.alphaAngle += 2; 
 
 }
