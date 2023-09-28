99140b9de85c416dcdb373eae2f85f8c4b779205
Heather Chester <h_cheste@live.concordia.ca>
2023-09-28 19:11:26 -0400

    attempted to create a fire back option with a "doubleClick" function

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -6,37 +6,53 @@
  */
 
 "use strict";
+//White circle becomes an alien 
+
+let alien = { 
+    x: 150,
+    y: 250, 
+    vx: 50,
+    vy: 50, 
+    size: 100, 
+    image: undefined 
+}
+
+let spaceship = { 
+    x: 0,
+    y: 250, 
+    size: 100, 
+    vx: 0, 
+    vy: 0, 
+    speed: 5, 
+    image: undefined 
+}
+
+let fire = { 
+    x: undefined, 
+    y: undefined, 
+    size: 5, 
+    vx: 0, 
+    vy: 0, 
+    speed: 5, 
+    fill: {
+        red: 255, 
+        green: 255, 
+        blue: 0
+    }
+
+}
+
 
 /**
  * Description of preload
 */
 function preload() {
 
+    alien.image = loadImage("assets/images/alien.png");
+    spaceship.image = loadImage("assets/images/spaceship.png");
 }
 
-let circle = {
-    x: 0,
-    y: 250, 
-    size: 100, 
-    vx: 0, 
-    vy: 0, 
-    speed: 5,
-    fill: {
-        r: 250, 
-        g: 100, 
-        b: 100, 
-        a: 255, 
-    }
-};
-
-let user = {
-    x: 150,
-    y: 250, 
-    size: 100, 
-    fill: 255
-};
-
-let numStatic = 500;
+let numStatic = 1000;
 
 
 /**
@@ -45,11 +61,8 @@ let numStatic = 500;
 function setup() {
     createCanvas (windowWidth, windowHeight);
 
-    circle.y = random(0,height);
-    circle.vx = circle.speed; 
-
-    noCursor();
-
+    spaceship.y = random(0,height);
+    spaceship.vx = spaceship.speed; 
 }
 
 
@@ -66,37 +79,69 @@ function draw() {
         stroke(255);
         point(x,y);
     } 
+
+    //spaceship movement 
+    spaceship.x = spaceship.x + spaceship.vx;
+    spaceship.y = spaceship.y + spaceship.vy; 
     
-    //Covid 19 movement 
-    circle.x = circle.x + circle.vx;
-    circle.y = circle.y + circle.vy; 
-
-    if (circle.x > width) {
-        circle.x = 0;
-        circle.y = random(0,height);
+    if (spaceship.x > width) {
+        spaceship.x = 0;
+        spaceship.y = random(0,height);
+    }
+    
+    //Conditionals for spaceship to chase alien vertically while it moves left to right
+    if (mouseX < spaceship.x) {
+        spaceship.vx = spaceship.speed;
+    }
+    if (mouseY < spaceship.y) { 
+        spaceship.vy = -spaceship.speed;
+    }
+    else { 
+        spaceship.vy = spaceship.speed;
     }
 
-    //User movement 
-    user.x = mouseX;
-    user.y = mouseY; 
 
-    //Check for catching covid 19 
-    let d = dist(user.x, user.y, circle.x, circle.y);
-    if (d < circle.size/2 + user.size/2) {
+    //alien movement 
+    alien.x = mouseX + alien.vx;
+    alien.y = mouseY + alien.vy; 
+
+
+    //Check for spaceship catching alien
+    let d = dist(alien.x, alien.y, spaceship.x, spaceship.y);
+    if (d < spaceship.size/4 + alien.size/4) {
+        noLoop (); 
+    }
+
+    //For alien firing 
+    function doubleClicked () {
+    if (spaceship.size > width);
+    rect(fire.x, fire.y, fire.size, fire.size)
+    fire.x = alien.x + fire.vx; 
+    fire.y = alien.y + fire.vy; 
+    fire.vx = -fire.speed; 
+    fire.vy = 0; 
+    }    
+
+    //Check for alien catching spaceship
+    let d2 = dist(spaceship.x, spaceship.y, fire.x, fire.y);
+    if (d2 < spaceship.size + fire) {
         noLoop ();
-
     }
+    
+    // Display spaceship   
+    noStroke();
+    fill(255, 0, 0);
+    image(spaceship.image, spaceship.x, spaceship.y, spaceship.size, spaceship.size);
 
-    // Display covid 19  
-    fill(circle.fill.r, circle.fill.g, circle.fill.b);
-    ellipse(circle.x, circle.y, circle.size);
+    //Display alien 
+    noStroke();
+    fill(255, 0, 0); 
+    image(alien.image, alien.x, alien.y, alien.size, alien.size,);
 
-    //Display user 
-    fill(user.fill); 
-    ellipse(user.x, user.y, user.size);
-
-
-
+    //Display fire
+    noStroke (); 
+    fill(fire.fill.red, fire.fill.green, fire.fill.blue); 
+    rect(fire.x, fire.y, fire.size, fire.size)
 
 
 }
