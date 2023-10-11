22d245c21a2a0a76002b0a2ced918ff26319a7e1
Heather Chester <h_cheste@live.concordia.ca>
2023-10-11 16:13:34 -0700

    Created title and ending cards + added the function mousePressed to switch from the states

--- project1/js/script.js
+++ project1/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * Project1 
+ * Heather Chester 
  * 
  * This is a template. You must fill in the title, author, 
  * and this description to match your project!
@@ -15,11 +15,13 @@ function preload() {
 
 }
 
+let state = `title`;
 
 /**
  * Description of setup
 */
 function setup() {
+    createCanvas(windowWidth, windowHeight);
 
 }
 
@@ -28,5 +30,46 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(0); 
 
+if (state === `title`) {
+    title();
+}
+
+// else if (state === `simulation`) {
+//     simulation(); 
+// }
+
+else if (state === `fin`) {
+    fin();
+}
+
+}
+
+function title() {
+    push(); 
+    textSize(64); 
+    fill(255); 
+    textAlign(CENTER, CENTER); 
+    text('title', width/2, height/2);
+    pop();
+}
+
+// function simulation() {
+    
+// }
+
+function fin() {
+    push();
+    textSize(64); 
+    fill(255); 
+    textAlign(CENTER, CENTER); 
+    text('fin', width/2, height/2); 
+    pop();
+}
+
+function mousePressed() {
+    if (state === `title`) {
+        state = `fin`;
+    }
 }
