61f5a061de11e96889c4d21ea33485679b797db5
Heather Chester <h_cheste@live.concordia.ca>
2023-10-22 22:50:48 -0400

    started debugging activity

--- activities/debugging/js/script.js
+++ activities/debugging/js/script.js
@@ -1,6 +1,6 @@
 /**
- * age-of-aquariums
- * Heather Chester 
+ * Title of Project
+ * Author Name
  * 
  * This is a template. You must fill in the title, author, 
  * and this description to match your project!
