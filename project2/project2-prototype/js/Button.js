159d419fe4fe4201083d7fbdfcab543df765b87e
Heather Chester <h_cheste@live.concordia.ca>
2023-11-08 22:50:37 -0500

    created sound visualizer based on  Nishān Wickramarathna code in their article, "Audio Visualization in JavaScript with p5.js" in Medium

--- project2/project2-prototype/js/Button.js
+++ project2/project2-prototype/js/Button.js
@@ -8,29 +8,19 @@ class Button {
         this.offColor = 140, 149, 150; // Grey for button is off 
         this.onColor = 81, 86, 87; // Darker grey for when button is on 
         this.buttonOn = false; 
-        // For sound 
-        this.note = [60, 62, 64, 65, 67, 69, 71] // Midi notes 
     }
     pressed() {
         // Function to detect if the button is on or off 
         if (!this.buttonOn) {
             this.buttonOn = true; 
-            this.playNote(this.note);  
+            // Fade notes in 
+            osc.fade(0.5, 0.2);
         }   
         else if (this.buttonOn) {
             this.buttonOn = false; 
-            this.endNote(); 
+            // Fade out of/ending a note
+            osc.fade(0, 0.2); 
         } 
-    }
-    playNote() {
-        // Function to play a note 
-        osc.freq(midiToFreq(this.note)); 
-        //Fade it in 
-        osc.fade(0.5, 0.2); 
-    }
-    endNote() {
-        // Function for fading out of notes 
-        osc.fade(0, 0.2); 
     }
 
     display() {
@@ -38,6 +28,7 @@ class Button {
         stroke(this.stroke);
         strokeWeight(this.strokeWeight);
         // If button is on display on color and if its off, display off color 
+        // On and off of button defined in pressed()
         if (!this.buttonOn) {
             fill(this.offColor);
         }
--- project2/project2-prototype/js/script.js
+++ project2/project2-prototype/js/script.js
@@ -11,15 +11,16 @@
  * Description of preload
 */
 
-let theramin; // 
 let osc; // To store oscillator 
+let fft; 
+let amp; 
+let lineSpacing; 
 
 let controller = {
     buttons: [],
-    numButtons: 7
+    numButtons: 7, 
+    note: [60, 62, 64, 65, 67, 69, 71] // Midi notes 
 };
-
-
 
 function preload() {
 }
@@ -30,16 +31,22 @@ function preload() {
 function setup() {
     createCanvas(windowWidth, windowHeight);
     userStartAudio();
-    // Create oscillator at 440Hz with a sawtooth waveform
-    // oscillator = new p5.Oscillator(440, `sawtooth`); 
+
+    // Sound
     osc = new p5.TriOsc(); 
     // Start silent 
     osc.start(); 
     osc.amp(0); 
 
+    // For sound visualization 
+    fft = new p5.FFT(0.9, 128);
+    angleMode(DEGREES);
+    lineSpacing = width/128; 
+
+    // Button setup 
     for (let i = 0; i < controller.numButtons; i++) { 
-        let x = i*100+25;
-        let y = height/1.2 
+        let x = i*width/7 + (width/14);
+        let y = height/1.2; 
         let button = new Button(x, y);
         controller.buttons.push(button);
     }
@@ -50,6 +57,15 @@ function setup() {
 */
 function draw() {
     background(110, 5, 5); // Dark Red
+
+    let spectrum = fft.analyze(); 
+    for (let j = 0; j < spectrum.length; j++) {
+        stroke(255);
+        let amp = spectrum[j];
+        let y = map(amp, 0, 256, height, 0); 
+        rect((width/2) + (j * lineSpacing), y, lineSpacing, height - y);
+        rect((width/2) - (j * lineSpacing), y, lineSpacing, height - y);
+    }
 
     // Show all the buttons 
     for (let i = 0; i < controller.buttons.length; i++) {
@@ -64,11 +80,9 @@ function mousePressed() {
             // If any buttons in the array have been pressed by the mouse 
             // Then tells the button class whether it is on or off, changing the display colour as well
         if (mouseX < button.x + button.size && mouseX > button.x && mouseY < button.y + button.size && mouseY > button.y) {
-            button.pressed(); 
+            button.pressed();
+            // Calls different notes according to the array index   
+            osc.freq(midiToFreq(controller.note[i])); 
         }
     }
 }
-
-// function mouseReleased() {
-    // theramin.stop(); 
-//}
