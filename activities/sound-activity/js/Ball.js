5f27a9ab74a46835d2842e2be4fc49c66fffb339
Heather Chester <h_cheste@live.concordia.ca>
2023-11-14 14:10:58 -0500

    added conditional checks and oscillator to program. Some bugs are evident although unsure of what it is yet.

--- activities/sound-activity/js/Ball.js
+++ activities/sound-activity/js/Ball.js
@@ -7,22 +7,47 @@ class Ball {
         this.vx = 0;
         this.vx = 0; 
         this.fill = {
-            r: 100, 
-            g: 100,
-            b: 100
+            r: 255, 
+            g: 255,
+            b: 255
         }
+        // Sound properties 
+        // this.osc = new p5.TriOsc(); 
+        // this.osc.amp(0.1); // oscillator amplitude 
+        // this.nearFreq = 220; 
+        // this.farFreq = 440; 
+        
     }
     move() {
         this.x = this.x + this.vx; 
         this.y = this.y + this.vy; 
+ 
+        // let this.newFreq = map(this.y, height, 0, 220, 440); 
     }
     bounce() {
-
+        if(this.x + this.size/2 > width) {
+            this.x = this.x - this.vx; 
+        }
+        else if(this.x - this.size/2 < 0) {
+            this.x = this.x + this.vx; 
+        }
+        else if(this.y + this.size/2 > height) {
+            this.y = this.y - this.vy; 
+        }
+        else if(this.y - this.size/2 < 0) {
+            this.y = this.y + this.vy; 
+        }
     }
     display() {
         push();
         fill(this.fill.r, this.fill.g, this.fill.b); 
         ellipse(this.x, this.y, this.size); 
         pop();
+    }
+    mousePressed() {
+        this.osc.start(); 
+    }
+    mouseReleased() {
+        this.osc.stop(); 
     }
 }
--- activities/sound-activity/js/script.js
+++ activities/sound-activity/js/script.js
@@ -9,6 +9,12 @@
 /**
  * Description of preload
 */
+
+let balls = []; // Empty array for balls 
+let totalBalls = 5; 
+
+let osc; // to store oscillator 
+
 function preload() {
 
 }
@@ -19,6 +25,15 @@ function preload() {
 */
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    // userStartAudio(); 
+
+    for (let i = 0; i < totalBalls; i++) {
+        let x = random(0, width); 
+        let y = random(0, height); 
+
+        let ball = new Ball(x, y); 
+        balls.push(ball); 
+    }
 
 }
 
@@ -29,4 +44,12 @@ function setup() {
 function draw() {
     background(0); 
 
+    for(let i = 0; i < balls.length; i++) {
+        let ball = balls[i]; 
+        ball.move(); 
+        // ball.bounce(); 
+        ball.display(); 
+        // ball.mousePressed(); // Calls Ball class for oscillator to start when mouse pressed 
+        // ball.mouseReleased(); // Calls Ball class for oscillator to stop when mouse released 
+    }
 }
