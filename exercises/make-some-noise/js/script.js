53606ee1765b6b9456be4e645176a71920de23d4
Heather Chester <h_cheste@live.concordia.ca>
2023-11-16 20:22:30 -0500

    Added some final comments

--- exercises/make-some-noise/js/script.js
+++ exercises/make-some-noise/js/script.js
@@ -2,7 +2,9 @@
  * Make-some-noise 
  * Heather Chester 
  * 
- *
+ * For this exercise, I decided to work on some of the feedback suggestions from the proposal submission. 
+ * Additionally, I switched up the buttons to resemble a piano and created a looping record button that operates on mouse operated states. 
+ * Please see README file for more information on how I plan on furthering this for project 2. 
  */
 
 "use strict";
@@ -11,6 +13,7 @@
  * Description of preload
 */
 
+// For all keys 
 let controller = {
     pianoKeys: [],
     sharpKeys: [],  
@@ -33,12 +36,13 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
     userStartAudio();
 
+    // Setup recorder 
     let x = width/2 - 50; 
     let y = height/3
-    recorder = new Recorder(x, y); 
-
+    recorder = new Recorder(x, y);
     
-
+    // Sets up piano and sharp array
+    // Assigns its note value per each object in array 
     for (let i = 0; i < controller.numPiano; i++) {
         let x = i*width/16 + (width/3.6)
         let y = height/2;
@@ -47,7 +51,7 @@ function setup() {
         pianoKey.oscillator.freq(midiToFreq(note)); 
         controller.pianoKeys.push(pianoKey); 
     }
-    // Created separate loops to create the separation between keys 
+    // Created separate loops to create the odd separation between sharp keys 
     for (let i = 0; i < controller.twoNumSharps ; i++) {
         let x = ((i*width/16) + width/3) - 22
         let y = height/2 - 30;
@@ -69,28 +73,32 @@ function setup() {
  * Description of draw()
 */
 function draw() {
-    background(255); 
-    // Shows white keys piano display
+    background(255);
+    // White keys piano display
     for (let i = 0; i < controller.pianoKeys.length; i++) {
         let pianoKey = controller.pianoKeys[i];
         pianoKey.pianoDisplay(); 
     }
+    // Black sharp keys display
     for (let i = 0; i < controller.sharpKeys.length; i++) {
         let sharpKey = controller.sharpKeys[i];
         sharpKey.sharpDisplay(); 
     }
+    // For recorder button display 
     recorder.display(); 
 }
 
 function mousePressed() {
-    // Calls for a mouse pressed check on the white piano keys 
+    // Check if the white piano keys have been pressed 
     for (let i = 0; i < controller.pianoKeys.length; i++) {
         let pianoKey = controller.pianoKeys[i]; 
         pianoKey.pressed(); 
-    }
+    } 
+    // Check for sharp keys 
     for (let i = 0; i < controller.sharpKeys.length; i++) {
         let sharpKey = controller.sharpKeys[i]; 
         sharpKey.pressed(); 
     }
+    // Check for recorder button 
     recorder.recorderPressed(); 
 }
