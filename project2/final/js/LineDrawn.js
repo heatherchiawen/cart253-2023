345222a7ff93cab4a3ab89e0f3e85faedfc510b1
Heather Chester <h_cheste@live.concordia.ca>
2023-11-22 17:05:29 -0500

    created a class to create a drawing program to replace user pressed keys

---
+++ project2/final/js/LineDrawn.js
@@ -0,0 +1,8 @@
+class LineDrawn {
+    constuctor() {
+        this.px = pwinMouseX; // pwin tracks the mouse pos. from the previous frame 
+        this.py = pwinMouseY; 
+        this.x = winMouseX; // mouse position in the current frame 
+        this.y = winMouseY; 
+    }
+}
